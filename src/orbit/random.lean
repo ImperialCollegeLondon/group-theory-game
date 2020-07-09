1fdf126b1b218ea125c91b94a51f8cde746c3f3c
Kexing <kexing.ying19@imperial.ac.uk>
2020-07-09 17:52:37 +0200

    more lemmas about the cardinality of partitions

--- src/orbit/random.lean
+++ src/orbit/random.lean
@@ -1,9 +1,22 @@
-import group_theory.subgroup data.fintype.basic
+import group_theory.subgroup data.fintype.basic 
+import tactic -- remove once library_search bug is fixed
 
 noncomputable theory
 
 open set classical
 local attribute [instance] prop_decidable
+
+structure partition (S : Type*) :=
+(blocks : set (set S))
+(nonempty : ∀ block ∈ blocks, block ≠ ∅)
+(covers : univ = ⋃ (s ∈ blocks), s)
+(disj   : ∀ s t ∈ blocks, s ∩ t ≠ ∅ → s = t)
+
+private lemma not_empty {α} {S : set α} (h : S ≠ ∅) : ∃ s : α, s ∈ S := 
+begin
+  by_contra hs, push_neg at hs,
+  exact (push_neg.not_eq _ _).1 h (set.eq_empty_iff_forall_not_mem.2 hs)
+end
 
 namespace action
 
@@ -12,7 +25,7 @@ structure laction (G : Type*) [group G] (S : Type*) :=
 (map_assoc : ∀ g h : G, ∀ s : S, to_fun g (to_fun h s) = to_fun (g * h) s)
 
 variables {G : Type*} [group G] {S : Type*}
-variables {α : Type*} {μ : laction G S}
+variables {μ : laction G S}
 
 -- Example of a left action - The natural left action of a group acting on itself
 def natural_self_laction : laction G G := 
@@ -25,18 +38,6 @@ def natural_self_laction : laction G G :=
 
 /-- An element of `G` is in its own orbit -/
 lemma self_mem_orbit (s : S) : s ∈ orbit μ s := ⟨1, (μ.2 s).symm⟩
-
-structure partition (S : Type*) :=
-(blocks : set (set S))
-(nonempty : ∀ block ∈ blocks, block ≠ ∅)
-(covers : univ = ⋃ (s ∈ blocks), s)
-(disj   : ∀ s t ∈ blocks, s ∩ t ≠ ∅ → s = t)
-
-private lemma not_empty {S : set α} (h : S ≠ ∅) : ∃ s : α, s ∈ S := 
-begin
-  by_contra hs, push_neg at hs,
-  exact (push_neg.not_eq _ _).1 h (set.eq_empty_iff_forall_not_mem.2 hs)
-end
 
 /-- The set of orbits of a set forms a partition -/
 def orbit_partition : partition S := 
@@ -93,7 +94,7 @@ lemma in_orbit_of_inv {s₁ s₂ : S} {g : G} (h : s₁ = μ.1 g s₂) :
 
 @[reducible]
 def is_conjugate (H K : subgroup G) := 
-  ∃ g : G, {c | ∃ h ∈ H, c = g⁻¹ * h * g } = K
+  ∃ g : G, { c | ∃ h ∈ H, c = g⁻¹ * h * g } = K
 
 /-- If `H` is the conjugate of `K`, then `K` is the conjugate of `H` -/
 lemma is_conjugate_comm {H K : subgroup G} (h : is_conjugate H K) :
@@ -133,12 +134,19 @@ end action
 
 namespace order
 
--- We need Lagrange's theorem for Orbit-Stabilizer theorem
--- In this section we will only consider finite groups
+-- We need Lagrange's theorem for Orbit-Stabilizer 
+
+/- We will use the cardinality function `fintype.card` defined for fintypes.
+While we will prove most theorems in this section for general groups, we will 
+only consider finite groups when considering the cardinality of the group in 
+question. This is reflected by the [fintype G] tag in theorems regarding 
+cardinality. 
+
+TODO : refactor the card stuff -/
 
 open function fintype
 
-variables {G : Type*} [group G] [fintype G] 
+variables {G : Type*} [group G]
 variables {H : subgroup G}
 
 @[reducible]
@@ -168,7 +176,7 @@ end
 -- A corollary of this is a • H = H iff a ∈ H 
 
 /-- The coset of `H`, `1 • H` equals `H` -/
-lemma lcoset_of_one : 1 • H = H :=
+theorem lcoset_of_one : 1 • H = H :=
 begin
   ext, split; intro hx,
     { rcases hx with ⟨h, hh₀, hh₁⟩,
@@ -179,6 +187,19 @@ end
 /-- A left coset `a • H` equals `H` if and only if `a ∈ H` -/
 theorem lcoset_of_mem {a : G} :
   a • H = H ↔ a ∈ H := by rw [←lcoset_of_one, lcoset_eq]; simp
+
+/-- Two left cosets `a • H` and `b • H` are equal if they are not disjoint -/
+theorem lcoset_digj {a b c : G} (ha : c ∈ a • H) (hb : c ∈ b • H) : 
+  a • H = b • H :=
+begin
+  rcases ha with ⟨g₀, hg₀, hca⟩,
+  rcases hb with ⟨g₁, hg₁, hcb⟩,
+  rw lcoset_eq, rw (show a = c * g₀⁻¹, by simp [hca, mul_assoc]),
+  rw (show b⁻¹ = g₁ * c⁻¹, 
+    by rw (show b = c * g₁⁻¹, by simp [hcb, mul_assoc]); simp),
+  suffices : g₁ * g₀⁻¹ ∈ H, simp [mul_assoc, this],
+  exact H.mul_mem hg₁ (H.inv_mem hg₀)
+end
 
 -- Now we would like to prove that all lcosets have the same order
 
@@ -192,8 +213,8 @@ begin
       suffices : (x : G) = y, 
         { ext, assumption },
         { simp [aux_map] at hxy, assumption } },
-    { rintro ⟨y, y_val⟩, 
-      rcases y_val with ⟨h, hh₀, hh₁⟩,
+    { rintro ⟨y, y_prop⟩, 
+      rcases y_prop with ⟨h, hh₀, hh₁⟩,
       refine ⟨⟨h, hh₀⟩, by simp [aux_map, hh₁]⟩ }
 end
 
@@ -202,15 +223,73 @@ theorem lcoset_equiv {a : G} : H ≃ a • H :=
 equiv.of_bijective (aux_map a H) aux_map_biject
 
 /-- The cardinality of `H` equals its left cosets-/
-lemma eq_card_of_lcoset {a : G} : card H = card (a • H) := 
+lemma eq_card_of_lcoset {a : G} [fintype G] : card H = card (a • H) := 
 begin
   rw card_eq, by_contra h,
   exact not_nonempty_iff_imp_false.1 h lcoset_equiv
 end
 
 /-- The cardinality of all left cosets are equal -/
-theorem card_of_lcoset_eq {a b : G} : card (a • H) = card (b • H) := 
-by iterate 2 { rw ←eq_card_of_lcoset }
+theorem card_of_lcoset_eq {a b : G} [fintype G] : 
+  card (a • H) = card (b • H) := by iterate 2 { rw ←eq_card_of_lcoset }
+
+/-- The left cosets of a subgroup `H` form a partition -/
+def lcoset_partition (G : Type*) [group G] (H : subgroup G) : partition G := 
+{ blocks := { B | ∃ g : G, B = g • H },
+  nonempty := λ B hB, let ⟨g, hg⟩ := hB in 
+    λ h, by rw [←(mem_empty_eq g), ←h, hg]; exact self_mem_coset g H,
+  covers := ext $ λ x, 
+    ⟨λ _, mem_Union.2 ⟨x • H, mem_Union.2 ⟨⟨x, rfl⟩, self_mem_coset x H⟩⟩, 
+      λ _, mem_univ x⟩,
+  disj := λ s t hs ht hndisj,
+      let ⟨g₀, hg₀⟩ := hs in 
+      let ⟨g₁, hg₁⟩ := ht in
+      let ⟨k, hks, hkt⟩ := not_empty hndisj in
+    begin
+      rw [hg₀, hg₁], 
+      rw hg₀ at hks, rw hg₁ at hkt, 
+      exact lcoset_digj hks hkt,
+    end }
+
+-- Now we prove that the card of a set equals the sum of the card of blocks
+
+private def aux_map' {α} [fintype α] (p : partition α) : 
+α → ⋃ (b ∈ p.blocks), b :=  λ a, ⟨a, p.3 ▸ mem_univ _⟩
+
+private lemma aux_map'_biject {α} [fintype α] (p : partition α) : 
+  bijective (aux_map' p) := 
+⟨λ _ _ _, by finish [aux_map'], λ ⟨y, y_prop⟩, ⟨y, by unfold aux_map'⟩⟩
+
+lemma Union_blocks_equiv {α} [fintype α] (p : partition α) : 
+  α ≃ ⋃ (b ∈ p.blocks), b :=
+equiv.of_bijective (aux_map' p) (aux_map'_biject p)
+
+/-- The cardinality of a fintype `α` equals the cardinality of its partition -/
+theorem eq_card_of_partition {α} [fintype α] (p : partition α) : 
+  card α = card ⋃ (b ∈ p.blocks), b :=
+begin
+  rw card_eq, by_contra h,
+  refine not_nonempty_iff_imp_false.1 h (Union_blocks_equiv p)
+end
+
+theorem card_Union {α} [fintype α] (p : partition α) [h : finite p.1] :
+card (⋃ (s ∈ h.to_finset), s) = h.to_finset.sum (λ s, card s) := 
+begin
+  sorry
+end
+
+theorem lagrange [fintype G] : 
+  card G = card { c | ∃ g : G, c = g • H } * card H := 
+begin
+  rw eq_card_of_partition (lcoset_partition G H),
+  /-
+  We need :
+  card (↥⋃ (b ∈ (lcoset_partition G H).blocks), b) = ...(1)
+  ∑ (b ∈ (lcoset_partition G H).blocks), card b     = ...(2)
+  card ↥{c : set G | ∃ (g : G), c = g • H} * card ↥H  ...(3)
+  -/
+  sorry
+end
 
 end order
 
