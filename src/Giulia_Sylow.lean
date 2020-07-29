8e325c4321f774d48f6e4cdb67b118af8719a42a
GiuliaCarfora <56804078+GiuliaCarfora@users.noreply.github.com>
2020-07-29 13:41:56 +0200

    various attempts at defining lemma

--- src/Giulia_Sylow.lean
+++ src/Giulia_Sylow.lean
@@ -60,12 +60,32 @@ begin
   ext x, 
   simp * at *, 
 end
---#exit
+localized "notation `∑` binders `, ` r:(scoped:67 f, finset.sum finset.univ f) := r" in big_operators
+localized "notation `∑` binders ` in ` s `, ` r:(scoped:67 f, finset.sum s f) := r" in big_operators
+variable [fintype G]
+lemma finite_orbit' [fintype G] {a : S} : finite (orbit μ a) :=
+begin
+  split, split,
+    swap, 
+    { apply finset.image,
+      exact λ g, ⟨μ.1 g a, g, rfl⟩,
+      exact (univ : set G).to_finset },
+    { simp, -- Nonterminating simp! I promise I will fix it!
+      rintro x ⟨g, rfl⟩,
+      refine ⟨g, rfl⟩ }
+end
+def foo  := {o | ∃ (s : S) (h : @card(orbit μ s) finite_orbit.fintype > 1), 
+@card (orbit μ s) finite_orbit.fintype = o}
+def foo2 := {o | ∃ s : S, orbit μ s = o}
+def foo3 (o) [h : fintype o] := card o  > 1
+#check foo
+lemma finite_foo : @finite ℕ (foo) := sorry
 --If S is a finite group then card S = card fixed_points G S + Σcard Oᵢ , 
 --where the sum runs over orbits of size > 1.
 lemma card_set_eq_card_fixed_points_sum_card_orbits (μ : laction G S)
- [fintype S] (ι : Type) (i : ι ): 
- card S = card(fixed_points μ) + sum(card(orbit μ s)) := sorry
+ [fintype S] : --how to write the different s_i in S? How to specify I only want the s_i 
+ --whose orbit has size > 1?
+ card S = card(fixed_points μ) + ∑ o in foo , id o := sorry
 --TODO: write adequate indexing and express sum correctly
 --set_option pp.implicit true 
 --If G is a group with card pⁿ, where p is prime and n ≥ 1, S is a finite set acted upon by G, 
