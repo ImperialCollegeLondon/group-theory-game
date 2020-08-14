8ce0d6466d917525869246658f4d3af68b8fa170
Kexing <kexing.ying19@imperial.ac.uk>
2020-08-14 02:32:50 +0200

    no errors

--- src/subgroup/lagrange.lean
+++ src/subgroup/lagrange.lean
@@ -1,60 +1,11 @@
-import hom.quotient data.setoid.partition
-#exit
+import hom.quotient data.setoid.partition for_mathlib.finsum
+
 /- I think we've decided to change:
 - fintype → is_finite
 - set.card → fincard
 -/
 
---open_locale classical big_operators
-
---notation `∑` binder ` ∈ ` s `, ` r:(scoped:67 f, finsum_in s f) := r
-localized "notation `∑` binder ` ∈ ` s `, ` r:(scoped:67 f, finsum_in s f) := r" in finsum
-localized "notation `∑` binders `, ` r:(scoped:67 f, finsum f) := r" in finsum
-
---localized "notation `∑` binders ` ∈ ` s `, ` r:(scoped:67 f, finsum_in s f) := r" in finsum
---localized "notation `∑` binders ` ∈ ` s `, ` r:(scoped:67 f, finsum_in s f) := r" in finsum
-
-open_locale finsum
-
 open setoid set
-
-namespace fincard
-
-variables {α β : Type} [comm_semiring β]
-
-lemma card_eq_sum_ones [is_finite α] (s : set α) : 
-  fincard s = ∑ x ∈ s, 1 := sorry
-
--- Question: we have is_finite α but we need finite s. Should we create an instance 
--- or should we not use set.sum at all-
-lemma sum_const [is_finite α] (s : set α) (m : β) :
-  ∑ x ∈ s, m = m * fincard s := 
-begin
-  rw card_eq_sum_ones, 
-  sorry
-end
-
-lemma sum_const_nat [is_finite α] {s : set α} {m : ℕ} {f : α → ℕ} (h : ∀ x ∈ s, f x = m) :
-  ∑ x ∈ s, f x = m * fincard s :=
-begin
-  sorry
-  /- have := sum_const s m,
-  norm_cast at this, -- This is annoying
-  rw ←this,
-  exact sum_ext rfl h -/
-end
-
-theorem card_eq_sum_partition [is_finite α] (s : set (set α)) (hS : is_partition s) : 
-  fincard α = ∑ x ∈ s, fincard x := 
-begin
-  sorry
-  /- rw [eq_card', ←hS.sUnion_eq_univ],
-  simp_rw eq_card,
-  unfold card,
-  rw sum_disjoint hS.pairwise_disjoint, refl, -/
-end
-
-end fincard
 
 namespace mygroup 
 
@@ -89,10 +40,11 @@ end
 
 /-- Let `H` be a subgroup of the finite group `G`, then the cardinality of `G` 
 equals the cardinality of `H` multiplied with the number of left cosets of `H` -/
-theorem lagrange [is_finite G] : 
-  fincard G = fincard H * fincard { B | ∃ g : G, B = lcoset g H } := 
+theorem lagrange [fintype G] : 
+  fincard' G = fincard' H * fincard' { B | ∃ g : G, B = lcoset g H } := 
 begin
-  rw card_eq_sum_partition _ (lcoset_partition H), sorry
+  sorry
+  --rw card_eq_sum_partition _ (lcoset_partition H), 
   --refine sum_const_nat (λ _ hx, _), 
   --rcases hx with ⟨g, rfl⟩, 
   --exact eq_card_of_lcoset.symm
