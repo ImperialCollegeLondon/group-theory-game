8f8f4074714c0c5e2f4560f677e1cf60bae195eb
GiuliaCarfora <56804078+GiuliaCarfora@users.noreply.github.com>
2020-08-28 15:34:48 +0200

    testing

--- src/orbit/orbit_stabilizer.lean
+++ src/orbit/orbit_stabilizer.lean
@@ -664,11 +664,12 @@ def conjugation_action [group G]: laction G (subgroup G) :=
     end  
      }
 
--- ∃ g : G, { c | ∃ h ∈ H, c = g⁻¹ * h * g }
 --Consider the group G acting on the set of all its subgroups by conjugation. Then stab H = norm H.
 -- Where do I specify the conjugation? SHOULD DO NORMALIZER EQ STABILIZER
+lemma mem_normalizer_iff [group G] {H : subgroup G} (x : G):
+  x ∈ normalizer H.carrier ↔ ∀ k : G, k ∈ H ↔ x * k * x⁻¹ ∈ H := by refl
 
-lemma normalizer_eq_stabilizer [group G] (H : subgroup G): 
+/-lemma normalizer_eq_stabilizer [group G] (H : subgroup G): 
 normalizer H.carrier = stabilizer (conjugation_action) H := 
 begin
   unfold conjugation_action,
@@ -696,12 +697,61 @@ begin
 
     {sorry},
     
-end  
+end  -/
 
 
 def normalizer' [group G] (H : subgroup G) :=
  stabilizer (conjugation_action) H
  
+
+lemma mem_normalizer'_def [group G] {H : subgroup G} (x : G) :
+  x ∈ normalizer' H ↔ {k : G | ∃ (h : G) (H : h ∈ H), k = x * h * x⁻¹} = H := subgroup.ext'_iff.symm
+
+lemma mem_normalizer'_iff [group G] {H : subgroup G} (x : G):
+  x ∈ normalizer' H ↔ ∀ k : G, k ∈ H ↔ x * k * x⁻¹ ∈ H := 
+begin
+  rw mem_normalizer'_def,
+  split,
+  intros hx s,
+  rw ext_iff at hx,
+  cases hx s with h1 h2,
+  split,
+  intro hs,
+  cases h2 hs,
+  cases h,
+  dsimp at *,
+  /-have test: s⁻¹ ∈ H ,
+   apply H.inv_mem hs,
+  have: x⁻¹ * s * x = w,
+    apply group.mul_left_cancel x,
+    rw ← group.mul_assoc,
+    rw ← group.mul_assoc,
+    rw group.mul_right_inv,
+    rw group.one_mul,
+    apply group.mul_right_cancel x⁻¹,
+    rw group.mul_assoc,
+    rw group.mul_right_inv,
+    rw group.mul_one,
+    assumption,-/
+  
+
+
+end  
+
+
+lemma normalizer_eq_stabilizer'' [group G] (H : subgroup G) : 
+(normalizer' H  : set G) = {g : G | ∀ n, n ∈ H ↔ g * n * g⁻¹ ∈ H} := 
+begin
+   ext1,
+   rw subgroup.mem_coe',
+   rw mem_normalizer'_def,
+   rw ext_iff,
+   split,
+   intro h,
+   sorry
+end
+
+
 
 lemma normalizer_eq_stabilizer' [group G] (H : subgroup G) : 
 (normalizer' H  : set G) = {g : G | ∀ n, n ∈ H ↔ g * n * g⁻¹ ∈ H} := 
@@ -719,26 +769,26 @@ begin
       unfold conjugation_action at hx,
       unfold conjugate_subgroup at hx,
       simp at *,
-      
-      sorry
+      rw ← hx,
+      rw ← subgroup.mem_coe,
+      dsimp,
+      use n,
+      split,
+      exact hn,
+      refl,
 
-  /-unfold normalizer',
-  unfold_coes,
-  unfold stabilizer,
-  unfold conjugation_action,
- -- unfold conjugate_subgroup,
-  norm_num,
-  ext,
-  split,
-  intro h,
-  norm_num,
-  intro n,
+  intro hxn,
+  unfold_coes at hx,
+  unfold normalizer' at hx,
+  unfold stabilizer at hx,
+  unfold conjugation_action at hx,
+  unfold conjugate_subgroup at hx,
   simp at *,
-  unfold conjugate_subgroup at h,
-  simp at *,
-  split,
-  intro hn,
-  sorry-/
+  
+
+      
+  sorry
+
 end  
 
 
