7a1598f8205a231b56a56ee8885bd8c46ff8329c
GiuliaCarfora <56804078+GiuliaCarfora@users.noreply.github.com>
2020-09-07 12:13:27 +0200

    Started proving Sylow 2, need to check some stuff

--- src/sylow/sylow_theorems.lean
+++ src/sylow/sylow_theorems.lean
@@ -528,11 +528,29 @@ lemma conjugates_eq_cardinality (g : G) (H : subgroup G) :
   fincard H = fincard (conjugate_subgroup g H) := 
 fincard.of_equiv (group_hom.mul_equiv_of_iso (conjugate_iso g H)).to_equiv
   
-/-def is_sylow_p_subgroup [fintype G] {p m n: ℕ} {hp : p.prime}{hG : fincard G = p ^ n * m}
- {hdiv : ¬ p ∣ m} (K : subgroup G): Prop := fincard K = p ^ n-/
+/-def is_sylow_p_subgroup [fintype G] {p m n: ℕ} (hp : p.prime)(hG : fincard G = p ^ n * m)
+ (hdiv : ¬ p ∣ m) (K : subgroup G): Prop := fincard K = p ^ n
 
--- theorem sylow_two [fintype G]{p : ℕ} {hp : p.prime} (H K : subgroup G) (h₁ : is_sylow_p_subgroup H hp)(h₂ : is_sylow_p_subgroup K hp) : 
--- ∃ (g : G), H = conjugate_subgroup g K  := sorry  
+def dumb_action2 (H K: subgroup G) : laction H (lcosets K) :=
+laction.comap (𝒾 H) (lcosets K) (dumb_action K)
+
+theorem sylow_two [fintype G]{p m n: ℕ} (hp : p.prime)(hG : fincard G = p ^ n * m)
+ (hdiv : ¬ p ∣ m) (H K : subgroup G) (h₁ : is_sylow_p_subgroup hp hG hdiv H)(h₂ : is_sylow_p_subgroup hp hG hdiv K) : 
+∃ (g : G), H = conjugate_subgroup g K  :=
+begin
+  have claim: ¬ p ∣ index H,
+  { unfold index,
+    unfold is_sylow_p_subgroup at h₁,
+    rw [hG, h₁, nat.mul_comm, nat.mul_div_assoc, nat.div_self, nat.mul_one],
+    exact hdiv,
+    apply nat.pow_pos (nat.prime.pos hp), 
+    simp},
+  have claim2: fincard (fixed_points (dumb_action' K)) ≠ 0,    
+  { sorry  },  
+  
+  sorry
+end    -/
+
 
 
 -- Consider the action of K on the set X of cosets of H in G μ: K × X → X, (y, xH) ↦ yxH. 
